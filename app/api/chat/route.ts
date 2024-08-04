118b3393816b7f014ac686ec3f9f099a7a4d1efb
Piyush Prateek <piyushprateek231996@gmail.com>
2024-08-05 00:58:31 +0530

    route.ts - added page.tsx - added ui global.scss- added  font kanit tailwind.config.ts - added fontfamily here too

---
+++ app/api/chat/route.ts
@@ -0,0 +1,73 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { DataAPIClient } from "@datastax/astra-db-ts";
+import { NextRequest } from "next/server";
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_KEY,
+});
+
+const client = new DataAPIClient(process.env.ASTRA_DB_APPLICATION_TOKEN);
+
+const endpoint = process.env.ASTRA_DB_API_ENDPOINT;
+if (!endpoint) {
+    throw new Error("ASTRA_DB_API_ENDPOINT is not defined");
+}
+
+const db = client.db(endpoint, {
+    namespace: process.env.ASTRA_DB_NAMESPACE,
+});
+
+export async function POST(req: NextRequest) {
+    try {
+        const { messages } = await req.json();
+
+        const latestMessage = messages[messages?.length - 1]?.content;
+        let docContext = "";
+
+        const { data } = await openai.embeddings.create({
+            input: latestMessage,
+            model: "text-embedding-3-small",
+        });
+
+        const collection = await db.collection("portfolio");
+
+        const cursor = collection.find({}, {
+            sort: {
+                $vector: data[0]?.embedding,
+            },
+            limit: 5,
+        });
+
+        const documents = await cursor.toArray();
+
+        docContext = `
+          START CONTEXT
+          ${documents?.map((doc) => doc.description).join("\n")}
+          END CONTEXT
+          `;
+
+        const ragPrompt = [
+            {
+                role: "system",
+                content: `
+              You are an AI assistant answering questions as Piyush Prateek in his Portfolio App. 
+              Format responses using markdown where applicable.
+              ${docContext}
+              If the answer is not provided in the context, the AI assistant will say, 
+              "I'am sorry, I do not know the answer".
+              `,
+            },
+        ];
+
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            stream: true,
+            messages: [...ragPrompt, ...messages],
+        });
+        const stream = OpenAIStream(response);
+        return new StreamingTextResponse(stream);
+    } catch (e) {
+        throw e;
+    }
+}
